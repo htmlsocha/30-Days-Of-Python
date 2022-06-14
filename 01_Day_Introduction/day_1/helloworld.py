974e242b014bd1e406e3ea1288752ba34620f8c9
AdrianPressly <a.socha@pressly.pl>
2022-06-14 16:10:16 +0200

    Day 1 Done

---
+++ 01_Day_Introduction/day_1/helloworld.py
@@ -0,0 +1,29 @@
+#check version
+import math
+
+
+print(983 + 9859342 + 9534892)
+print(9852 - 8935 + (8528 * 203))
+print(17%5 / 8 ** 4 )
+
+print('Adrian')
+print('Socha')
+print('Poland')
+print('I am enjoying 30 days of python')
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Asabeneh','Python','Finland']))
+print(type('Adrian'))
+print(type('Socha'))
+print(type('Poland'))
+print(type(17%5/8*4))
+print(type(4-2j))
+
+a = [2,3]
+b = [10,8]
+
+
+print(math.sqrt((a[0]-b[0])**2+(a[1]-b[1])**2)) # Euclidean Distance between X = (2,3) and Y = (10,8)
